bca81099fc4cd3e685f4b33d3fbf137b518b3cd4
Yaseen Saleem <yaseen.saleem@trapezegroup.com>
2020-07-13 20:37:37 +0500

    Merge Loacal with master

---
+++ ClientBooking/Repository/EmailLogRepository.cs
@@ -0,0 +1,30 @@
+﻿using ClientBooking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientBooking.Repository
+{
+    public class EmailLogRepository : IEmailLogRepository
+    {
+        private ClientBookingContext EmailLogDB;
+
+        public EmailLogRepository()
+        {
+            EmailLogDB = new ClientBookingContext() ?? throw new ArgumentNullException(nameof(EmailLogDB));
+        }
+
+        public void EmailLog(string Email, DateTime Date, int BookingId)
+        {
+            EmailLog emailLog = new EmailLog()
+            {
+                Email = Email,
+                Date = DateTime.Today,
+                BookingId = BookingId
+            };
+            EmailLogDB.EmailLog.Add(emailLog);
+            EmailLogDB.SaveChanges();
+        }
+    }
+}
